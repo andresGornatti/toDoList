727df421819eec722c283cbc03b40eb9e5d8dbad
gornattiandres <gornattiandres@gmail.com>
2020-02-11 15:54:00 -0300

    Robofriends App: WebDev Course React Excercise

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './containers/App';
+import "tachyons";
 import './index.css';
-import App from './App';
+//import Card from "./Card";
+//import CardList from "./CardList";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
