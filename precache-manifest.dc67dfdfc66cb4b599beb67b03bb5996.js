19ad8faacb9450d400bac8cb9a08b63cab96f046
gornattiandres <gornattiandres@gmail.com>
2020-02-22 19:22:45 -0300

    Updates

---
+++ precache-manifest.dc67dfdfc66cb4b599beb67b03bb5996.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01afe62dbc515f8064ce463a60181eb",
+    "url": "/toDoList/index.html"
+  },
+  {
+    "revision": "f906c31cfca1ea34b96b",
+    "url": "/toDoList/static/css/main.b2478532.chunk.css"
+  },
+  {
+    "revision": "4f5628f310a28219d6cc",
+    "url": "/toDoList/static/js/2.ffe8c356.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/toDoList/static/js/2.ffe8c356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f906c31cfca1ea34b96b",
+    "url": "/toDoList/static/js/main.ceef8044.chunk.js"
+  },
+  {
+    "revision": "9ce8edc73c76c9ec31e0",
+    "url": "/toDoList/static/js/runtime-main.37caf175.js"
+  },
+  {
+    "revision": "32613961fef4d0a0dd68c7af12980cd6",
+    "url": "/toDoList/static/media/fontello.32613961.ttf"
+  },
+  {
+    "revision": "87aebd66b8fe501241ea5ba683e60f1f",
+    "url": "/toDoList/static/media/fontello.87aebd66.woff"
+  }
+]);
