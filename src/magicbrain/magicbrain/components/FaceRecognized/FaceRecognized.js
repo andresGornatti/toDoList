eef9e59ff30daf14ab4a98b63bceb8c82c550224
gornattiandres <gornattiandres@gmail.com>
2020-03-05 17:20:54 -0300

    Deploy!

--- src/magicbrain/magicbrain/components/FaceRecognized/FaceRecognized.js
+++ src/magicbrain/magicbrain/components/FaceRecognized/FaceRecognized.js
@@ -1,5 +1,4 @@
 import React from "react";
-//import "./FaceRecognized.css";
 
 const FaceRecognized = () => {
 		return (
--- src/magicbrain/magicbrain/components/SignIn/SignIn.js
+++ src/magicbrain/magicbrain/components/SignIn/SignIn.js
@@ -1,6 +1,5 @@
 
 import React from "react";
-//import "./SignIn.css";
 
 class SignIn extends React.Component {
   constructor(props){
